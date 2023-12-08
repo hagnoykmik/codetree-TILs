94cfb1892454941b7e5e6fa9528eaf274890600a
kimkyongah <109258144+hagnoykmik@users.noreply.github.com>
2023-12-09 01:08:59 +0900

    Title: 포탑 부수기, Time: 136ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231209/포탑 부수기/destroy-the-turret.py
+++ 231209/포탑 부수기/destroy-the-turret.py
@@ -2,7 +2,7 @@ from collections import deque
 
 # 공격할 포탑과 공격대상 포탑 선정하기
 def select():
-    global sx, sy, goal_x, goal_y
+    global sx, sy, goal_x, goal_y, end
     turret = []
 
     for x in range(N):
@@ -11,6 +11,10 @@ def select():
             if board[x][y] != 0:
                 # 공격력, 행, 열
                 turret.append((board[x][y], x, y))
+
+    if len(turret) == 1:
+        end = True
+        return
 
     turret.sort(key=lambda x: (x[0], -(x[1] + x[2]), -x[2]))
 
@@ -39,7 +43,7 @@ def select():
             cx, cy = turret[j][1], turret[j][2]
             if turret[j][0] == goal_turret[0]:
                 if (goal_x, goal_y) in attack_list and (cx, cy) in attack_list:
-                    if attack_list[(sx, sy)] < attack_list[(cx, cy)]:
+                    if attack_list[(goal_x, goal_y)] > attack_list[(cx, cy)]:
                         goal_x, goal_y = cx, cy
                 elif (cx, cy) in attack_list:
                     goal_x, goal_y = cx, cy
@@ -142,7 +146,7 @@ turret = []
 attack_list = dict() # 공격한 포탑 저장
 result = 0
 
-# 만약 부서지지 않은 포탑이 1개가 된다면 그 즉시 중지됩니다.
+end = False # 만약 부서지지 않은 포탑이 1개가 된다면 그 즉시 중지됩니다.
 for k in range(K):
 
     sx, sy = -1, -1
@@ -150,10 +154,13 @@ for k in range(K):
 
     # 맨 앞에 있는게 공격할 대상
     select()
+    if end:
+        print(board)
+        break
+
     attack_list[(sx, sy)] = k  # k번째에 공격함(숫자가 클수록 최신)
     board[sx][sy] += N + M  # 공격력 초기화
     score = board[sx][sy]
-
 
     # 공격
     attacked_list = attack(sx, sy)
@@ -164,13 +171,14 @@ for k in range(K):
     # 포탑 부서짐
     crush()
 
-
     # 포탑 정비
     repair()
-
+    print(board)
 
 for line in board:
     point = max(line)
     if point > result:
         result = point
+
 print(result)
+print(attack_list)
